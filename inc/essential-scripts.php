81aabcdb57a55029c76b427546bd1132a51bfdb1
sarthak garg <Sarthakgarg1122@gmail.com>
2026-05-23 15:13:30 +0530

    feat: implement essential script and style enqueuing for theme assets and Elementor integration

--- inc/essential-scripts.php
+++ inc/essential-scripts.php
@@ -105,16 +105,21 @@ function quanto_essential_scripts() {
             }
         }
         
-        // Ensure Elementor's core styles and scripts are enqueued
-        wp_enqueue_style( 'elementor-icons' );
-        wp_enqueue_style( 'elementor-frontend' );
-        wp_enqueue_style( 'elementor-post-' . $homepage_id );
-        wp_enqueue_style( 'elementor-global' );
-        
-        // Let Elementor know it's needed
+        // Let Elementor know it's needed and trigger widget CSS enqueueing for the homepage elements
         if ( class_exists( '\Elementor\Plugin' ) ) {
+            \Elementor\Plugin::$instance->frontend->has_elementor_in_page( true );
             \Elementor\Plugin::$instance->frontend->enqueue_styles();
             \Elementor\Plugin::$instance->frontend->enqueue_scripts();
+            
+            // Manually enqueue common Elementor and widget styles that might be missed
+            wp_enqueue_style( 'e-flexbox' );
+            wp_enqueue_style( 'elementor-widget-heading' );
+            wp_enqueue_style( 'elementor-widget-text-editor' );
+            wp_enqueue_style( 'elementor-widget-icon-list' );
+            wp_enqueue_style( 'elementor-widget-image' );
+            wp_enqueue_style( 'elementor-widget-button' );
+            wp_enqueue_style( 'elementor-widget-divider' );
+            wp_enqueue_style( 'elementor-widget-spacer' );
         }
     }
 
