957bc6bba73ba82ead5c0d217fef0d6e0f705975
sarthak garg <Sarthakgarg1122@gmail.com>
2026-05-31 12:37:56 +0530

    feat: add cmr_spotlight shortcode to render media coverage logo grid

--- inc/cmr-spotlight.php
+++ inc/cmr-spotlight.php
@@ -25,38 +25,38 @@ function cmr_render_spotlight_shortcode( $atts ) {
 
             <!-- Row 1 Logos -->
             <div class="cmr-spotlight-cell r1-c4">
-                <img src="https://www.google.com/s2/favicons?domain=timesofindia.indiatimes.com&sz=128" alt="TOI" style="max-height:60px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/The_Times_of_India_logo.png?width=320" alt="TOI" style="max-height:60px;">
             </div>
             <div class="cmr-spotlight-cell r1-c5">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/CNBC_logo.svg" alt="CNBC" style="max-height:50px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/CNBC_logo.svg?width=320" alt="CNBC" style="max-height:50px;">
             </div>
 
             <!-- Row 2 Logos -->
             <div class="cmr-spotlight-cell r2-c1">
-                <img src="https://www.google.com/s2/favicons?domain=ndtv.com&sz=128" alt="NDTV" style="max-height:60px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/NDTV_logo.svg?width=320" alt="NDTV" style="max-height:60px;">
             </div>
             <div class="cmr-spotlight-cell r2-c2">
-                <img src="https://www.google.com/s2/favicons?domain=news.abplive.com&sz=128" alt="ABP News" style="max-height:60px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/ABP_News_logo.svg?width=320" alt="ABP News" style="max-height:60px;">
             </div>
             <div class="cmr-spotlight-cell r2-c3">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/BBC_News_2019.svg" alt="BBC News" style="max-height:40px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/BBC_News_2019.svg?width=320" alt="BBC News" style="max-height:40px;">
             </div>
             <div class="cmr-spotlight-cell r2-c4">
-                <img src="https://www.google.com/s2/favicons?domain=aajtak.in&sz=128" alt="Aaj Tak" style="max-height:60px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/Aaj_tak_logo.png?width=320" alt="Aaj Tak" style="max-height:60px;">
             </div>
             <div class="cmr-spotlight-cell r2-c5">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/CNN.svg" alt="CNN" style="max-height:45px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/CNN.svg?width=320" alt="CNN" style="max-height:45px;">
             </div>
             <div class="cmr-spotlight-cell r2-c6">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/India_Today_logo.png" alt="India Today" style="max-height:45px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/India_Today_logo.png?width=320" alt="India Today" style="max-height:45px;">
             </div>
 
             <!-- Row 3 Logos -->
             <div class="cmr-spotlight-cell r3-c2">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Zee_News_Logo_2025.svg/330px-Zee_News_Logo_2025.svg.png" alt="Zee News" style="max-height:45px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/Zee_News_Logo_2025.svg?width=320" alt="Zee News" style="max-height:45px;">
             </div>
             <div class="cmr-spotlight-cell r3-c3">
-                <img src="https://www.google.com/s2/favicons?domain=republicbharat.com&sz=128" alt="Republic Bharat" style="max-height:60px;">
+                <img src="https://en.wikipedia.org/wiki/Special:FilePath/Republic_Bharat_Logo.svg?width=320" alt="Republic Bharat" style="max-height:60px;">
             </div>
 
         </div>
