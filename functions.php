05434908b1e40b415490298a0a7510f7b9f72fe0
sarthak garg <Sarthakgarg1122@gmail.com>
2026-06-15 14:53:38 +0530

    feat: add cmr_live shortcode to display recent post grids

--- functions.php
+++ functions.php
@@ -119,6 +119,7 @@ require_once QUANTO_DIR_PATH_INC . 'cmr-industry-intelligence-trends.php';
 require_once QUANTO_DIR_PATH_INC . 'cmr-industry-stack.php';
 require_once QUANTO_DIR_PATH_INC . 'cmr-market-updates-hero.php';
 require_once QUANTO_DIR_PATH_INC . 'cmr-market-updates-insights.php';
+require_once QUANTO_DIR_PATH_INC . 'cmr-live.php';
 require_once QUANTO_DIR_PATH_INC . 'cmr-breadcrumbs.php';
 require_once QUANTO_DIR_PATH_INC . 'cmr-sticky-nav-script.php';
 require_once QUANTO_DIR_PATH_INC . 'cmr-ajax-handlers.php';
