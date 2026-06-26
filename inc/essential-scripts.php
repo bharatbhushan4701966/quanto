ba21b5acd512011ce30d1f853c090a3b764f931a
sarthak garg <Sarthakgarg1122@gmail.com>
2026-06-26 15:25:25 +0530

    Add custom CSS for WooCommerce checkout block

--- inc/essential-scripts.php
+++ inc/essential-scripts.php
@@ -57,6 +57,7 @@ function quanto_essential_scripts() {
     
     // CMR News Style (loaded globally so footer overrides in it reflect on all pages)
     wp_enqueue_style( 'cmr-news-style', get_theme_file_uri('/assets/css/cmr-news.css') ,array(), time() );
+    wp_enqueue_style( 'cmr-checkout-block', get_theme_file_uri('/assets/css/cmr-checkout-block.css') ,array(), time() );
     wp_register_style( 'cmr-latest-insights', get_theme_file_uri('/assets/css/cmr-latest-insights.css'), array(), time() );
     wp_register_style( 'cmr-industry-intelligence', get_theme_file_uri('/assets/css/cmr-industry-intelligence.css'), array(), time() );
     wp_register_style( 'cmr-explore-sectors', get_theme_file_uri('/assets/css/cmr-explore-sectors.css'), array(), time() );
