2f6c355517ee480e98244a8126363fcee011971e
abu-rajput <abrajput468@gmil.com>
2026-09-08 10:47:43 +0530

    fix: include 404 pages in header template conditional logic

--- templates/header.php
+++ templates/header.php
@@ -14,8 +14,8 @@
 
     if ( class_exists( 'ReduxFramework' ) && defined( 'ELEMENTOR_VERSION' ) ) {
 
-        // ✅ Handle archive, blog, Single Post and search pages first
-        if ( is_archive() || is_home() || is_search() || is_singular( ['post', 'cmr_news', 'cmr_media', 'cmr_media_release', 'cmr_quarterly', 'product', 'quanto_job', 'team', 'teams', 'quanto_team'] ) ) {
+        // ✅ Handle 404, archive, blog, Single Post and search pages first
+        if ( is_404() || is_archive() || is_home() || is_search() || is_singular( ['post', 'cmr_news', 'cmr_media', 'cmr_media_release', 'cmr_quarterly', 'product', 'quanto_job', 'team', 'teams', 'quanto_team'] ) ) {
 
             $header_post = get_page_by_path( 'blog-header', OBJECT, 'quanto_header' );
             
