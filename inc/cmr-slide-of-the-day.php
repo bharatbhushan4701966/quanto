cc4c865637449d274af041c1a205707c996903cf
abu-rajput <abrajput468@gmil.com>
2026-09-08 15:08:31 +0530

    style: remove background color from hero-section in cmr-slide-of-the-day.php

--- inc/cmr-slide-of-the-day.php
+++ inc/cmr-slide-of-the-day.php
@@ -17,7 +17,6 @@ function cmr_slide_of_the_day_shortcode( $atts ) {
         
         /* Hero Dark Section */
         .hero-section {
-            background-color: var(--bg-dark);
             position: relative;
             padding-top: 50px;
             padding-bottom: 50px;
