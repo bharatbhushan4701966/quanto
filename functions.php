23a91960e38434a1245a3e401a884f255884e064
abu-rajput <abrajput468@gmil.com>
2026-09-09 15:02:10 +0530

    style: hide element d155fe5 on mobile devices via responsive CSS overrides

--- functions.php
+++ functions.php
@@ -3260,6 +3260,14 @@ add_action( 'wp_head', function() {
         }
 
         @media (max-width: 768px) {
+            .elementor-element.elementor-element-d155fe5,
+            div[data-id="d155fe5"],
+            .elementor-element-d155fe5.e-con,
+            .elementor-element-d155fe5.e-div-block-base,
+            .e-d155fe5-f948194 {
+                display: none !important;
+            }
+
             .elementor-element-b06ac09,
             div[data-id="b06ac09"] {
                 display: flex !important;
