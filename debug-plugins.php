04993fd1865c246716cd0af7362503dff8d87579
sarthak garg <Sarthakgarg1122@gmail.com>
2026-09-22 09:41:01 +0530

    debug: view process.php

--- debug-plugins.php
+++ debug-plugins.php
@@ -2,18 +2,10 @@
 require_once( __DIR__ . '/../../../wp-load.php' );
 header('Content-Type: text/plain');
 
-$plugin_dir = WP_PLUGIN_DIR . '/quanto-core';
-echo "Plugin dir: " . $plugin_dir . "\n";
-
-if (is_dir($plugin_dir)) {
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($plugin_dir));
-    foreach ($iterator as $file) {
-        if ($file->isFile() && $file->getExtension() === 'php') {
-            $content = file_get_contents($file->getPathname());
-            if (stripos($content, 'quanto_process') !== false || stripos($content, 'process-box') !== false) {
-                echo "Found matching file: " . $file->getPathname() . "\n";
-            }
-        }
-    }
+$file = WP_PLUGIN_DIR . '/quanto-core/addons/widgets/process.php';
+if (file_exists($file)) {
+    echo file_get_contents($file);
+} else {
+    echo "File not found!";
 }
 
